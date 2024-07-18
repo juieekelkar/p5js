b5e6b6e1c1a194e97eb0aa65cfab8d120d3511ee
juieekelkar <juieekelkar25@gmail.com>
2024-07-18 13:07:15 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(700, 480);
+  
+  //R,G and B values.
+  background(225,204,0);
+  
+  //take a big brush
+  strokeWeight(30);
+  //dip it in color purple
+  stroke('#3C0897');
+  
+  //place a dot on the screen
+  point(700/2,480/2);
+}
+function draw () {}
